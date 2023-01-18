072cbdd7615534d40c850d037a3709a6f22ddc5b
sinzanrin <niiyama.business@gmail.com>
2023-01-18 15:03:15 +0900

    修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,7 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, only: [:edit,]
+  before_action :set_prototype, except: [:index, :new, :create]
 
   def index
     @prototypes = Prototype.includes(:user)
@@ -50,8 +51,12 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
   def move_to_index
-    unless 
+    unless user_signed_in? 
       redirect_to root_path
     end
   end
